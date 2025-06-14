d2cf177b16e8ae198cfa51b45c7ef670fb53f608
NeMoWek <artemmarkov10000@gmail.com>
2025-06-14 19:37:40 +0300

    BrainPRime

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import Bodygames from '../src/bodygames.js'
+import BrainPrime from '../src/games/prime.js'
+
+Bodygames('Answer "yes" if given number is prime. Otherwise answer "no".', BrainPrime)
---
+++ src/games/prime.js
@@ -0,0 +1,14 @@
+const BrainPrime = () => {
+  const num = Math.round(Math.random() * 100) + 2
+  const numD = Math.round(Math.sqrt(num))
+
+  for (let i = 2; i <= numD; i += 1) {
+    if (num % i === 0) {
+      return [num, 'no']
+    }
+  }
+
+  return [num, 'yes']
+}
+
+export default BrainPrime
