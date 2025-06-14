7c882f59c362d846c52cf47627570ccc34186271
NeMoWek <artemmarkov10000@gmail.com>
2025-06-14 19:30:05 +0300

    BrainGCD

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import Bodygames from '../src/bodygames.js'
+import BrainGCD from '../src/games/gcd.js'
+
+Bodygames('Find the greatest common divisor of given numbers.', BrainGCD)
