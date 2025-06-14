f16f1df13b59d6eff79aa875bf44eeed56a637eb
NeMoWek <artemmarkov10000@gmail.com>
2025-06-14 19:44:28 +0300

    fix2

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import Bodygames from '../src/bodygames.js'
-import BrainPrime from '../src/games/prime.js'
+import brainPrime from '../src/games/prime.js'
 
-Bodygames('Answer "yes" if given number is prime. Otherwise answer "no".', BrainPrime)
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+Bodygames(description, brainPrime)
