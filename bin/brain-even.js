dc5ff8a5b07f5b597cdfb3d634ccb05d1382efb9
NeMoWek <artemmarkov10000@gmail.com>
2025-06-14 18:50:16 +0300

    Brain-Even + Тело для игр

---
+++ bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import Bodygames from '../src/bodygames.js'
+import task1 from '../src/games/even.js'
+
+Bodygames('Answer "yes" if the number is even, otherwise answer "no".', task1)
---
+++ src/bodygames.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync'
+import welcome from './cli.js'
+
+const Bodygames = (description, taskFunc) => {
+  const PName = welcome()
+  console.log(description)
+  let mark = 0
+  
+  for (let i = 1; i <= 3; i += 1) {
+    const step = taskFunc()
+
+    console.log(`Question: ${step[0]}`)
+    const answer = readlineSync.question('Answer: ')
+
+    if (answer === step[1]) {
+      console.log('Correct!')
+      mark += 1
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${step[1]}'.`)
+      console.log(`Let's try again, ${PName}!\n`)
+      break
+    }
+  }
+
+  if (mark === 3) {
+    console.log(`Congratulations, ${PName}!`)
+  }
+}
+
+export default Bodygames
