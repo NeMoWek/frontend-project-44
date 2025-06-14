a3e212738bc00839ac57967fe71eba6b0e3df1e0
NeMoWek <artemmarkov10000@gmail.com>
2025-06-14 19:34:01 +0300

    BrainP....

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import Bodygames from '../src/bodygames.js'
+import BrainProgression from '../src/games/progression.js'
+
+Bodygames('What number is missing in the progression?', BrainProgression)
